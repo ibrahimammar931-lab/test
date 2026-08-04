a6f5609ffcf2b9b6d5509ea48293db388d4f2e2b
Ibrahim Ammar <ibrahimammar931@gmail.com>
2026-08-04 10:15:18 +0100

    KAN-29 Add a health check endpoint

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
-
 from .database import Base, engine
-from .routes import authors, books
+from .routes import authors, books, health
 
 Base.metadata.create_all(bind=engine)
 
@@ -9,7 +8,7 @@ app = FastAPI(title="Bookstore API")
 
 app.include_router(authors.router)
 app.include_router(books.router)
-
+app.include_router(health.router)
 
 @app.get("/")
 def root():
